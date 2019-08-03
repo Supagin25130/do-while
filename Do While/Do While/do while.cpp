739311f2472e99a35182193405f83414321120f0
Supagin25130 <52159977+Supagin25130@users.noreply.github.com>
2019-08-03 21:16:14 +0700

    Add files via upload

---
+++ Do While/Do While/do while.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char choose;
+	string user, user1;
+	int pass, pass1;
+	do {
+		cout << "//////////////Menu//////////////" << endl;
+		cout << "1. Register" << endl;
+		cout << "2. Login" << endl;
+		cout << "Q. Exit" << endl;
+		cout << "Enter Menu : ";
+		cin >> choose;
+		if (choose == '1')
+		{
+			cout << "**********Register***********" << endl;
+			cout << "Input Usename : ";
+			cin >> user;
+			cout << "Input Password : ";
+			cin >> pass;
+		}
+		else if (choose == '2')
+		{
+			cout << "***********Login************" << endl;
+			cout << "Input Username : ";
+			cin >> user1;
+			cout << "Input password : ";
+			cin >> pass1;
+			if (user == user1 && pass == pass1)
+			{
+				cout << "Username or Password Correct ^_^" << endl;
+			}
+			else
+			{
+				cout << "!!!!Username or Password incorrect Please try again!!!!" << endl;
+			}
+		}
+		}while (choose != 'Q' && choose != 'q');
+		cout << "Exit Program" << endl;
+}
+
